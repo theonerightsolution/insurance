3f185e3496a8b09eaf6899f4bd7cc2c385f0afbd
prathamspace <prathamonweb@gmail.com>
2024-11-21 22:49:34 +0530

    intital office design

--- mail.php
+++ mail.php
@@ -5,10 +5,9 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $services = $_POST['services'];
 $message = $_POST['message'];
-$formcontent="First Name: $firstn \n Last Name: $lastn \n Email: $email \n Services: $services \n Phone: $phone \n Message: $message";
-$recipient = "mdsubhan.53@gmail.com";
+$formcontent = "First Name: $firstn \n Last Name: $lastn \n Email: $email \n Services: $services \n Phone: $phone \n Message: $message";
+$recipient = "";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 require_once "thank-you.html";
-?>
